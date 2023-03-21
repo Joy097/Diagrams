174566189d7972b4aa8924a19ad6f6818a9c5aaf
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-21 14:55:40 +0600

    update 3

--- FinalDiagram.py
+++ FinalDiagram.py
@@ -15,37 +15,13 @@ with Diagram("Neir and Open-api Architecture", show=True):
     with Cluster("Internal Network"):
         
         with Cluster("Load balancer"):
-            lb = EKS("172.16.254.26(gzvldopenapi02)")
+            lb = EKS("172.16.254.26\n(gzvldopenapi02)")
         
         with Cluster("IDM"):
-            idm = [ ECS("172.16.254.25:8080(gzvldopenapi01)"),
-                    ECS("172.16.254.52:8080(gzvlam01)")]
+            idm = [ ECS("172.16.254.25:8080\n(gzvldopenapi01)"),
+                    ECS("172.16.254.52:8080\n(gzvlam01)")]
 
-        with Cluster("API Gateway"):
-            apiG = [ECS("172.16.254.122(veonapi)"),
-                    ECS("172.16.254.25(gzvldopenapi01)"),
-                    ECS("172.16.254.52(gzvlam01)")]
-
-        with Cluster("LAN"):
-            lan = [ Lambda("172.16.7.90(gzvlapihdev02)"),
-                    Lambda("172.16.7.90(gzvllopenapi01)"),
-                    Lambda("172.16.7.90(gzvllopenapi02)"),
-                    Lambda("172.16.7.90(gzvlapihub15)")]
-            
-        with Cluster("Business Layer"):
-            bl = [ECS("Identity-Register(N)"),
-                  ECS("CMS(OA)"),
-                  ECS("LMS(OA)"),
-                  ECS("IRIS(OA)"),]
-            
-        with Cluster("Integration Layer(AH & BTRC)"):
-            ew = [  ECS("BTRC"),
-                    ECS("EIR"),
-                    ECS("Customer-Information"),
-                    ECS("IRIS"),
-                    ECS("CMS2"),
-                    ECS("LMS2")]
-
+        
     
 
-    ex_clnt >> lb >> idm >> apiG >> lan >> bl >> ew
+    ex_clnt >> lb >> idm
