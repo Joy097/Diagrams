9936fa2e6fa7a35553adadd033154630475f0eca
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-21 15:05:43 +0600

    update 4

--- FinalDiagram.py
+++ FinalDiagram.py
@@ -20,8 +20,22 @@ with Diagram("Neir and Open-api Architecture", show=True):
         with Cluster("IDM"):
             idm = [ ECS("172.16.254.25:8080\n(gzvldopenapi01)"),
                     ECS("172.16.254.52:8080\n(gzvlam01)")]
+            
+        with Cluster("API Gateway"):
+            apiG = [ECS("172.16.254.122\n(veonapi)"),
+                    ECS("172.16.254.25\n(gzvldopenapi01)"),
+                    ECS("172.16.254.52\n(gzvlam01)")]
+
+        with Cluster("LAN"):
+            lan = [ Lambda("172.16.7.90(gzvlapihdev02)"),
+                    Lambda("172.16.7.90(gzvllopenapi01)"),
+                    Lambda("172.16.7.90(gzvllopenapi02)"),
+                    Lambda("172.16.7.90(gzvlapihub15)")]
+
+            
 
         
     
 
     ex_clnt >> lb >> idm
+    lb >> apiG
