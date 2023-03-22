2546d816193d90355dc1793f414bfb819becb0cb
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-22 17:03:09 +0600

    update 41

--- ruff.py
+++ ruff.py
@@ -1,10 +1,16 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
+from diagrams import Diagram, Cluster, Edge
+from diagrams.digitalocean.compute import Droplet
+from diagrams.digitalocean.database import DbaasPrimary
+from diagrams.elastic.elasticsearch import Logstash
 
-graph_attr = {
-    "fontsize": "45",
-    "bgcolor": "transparent"
-}
+with Diagram("My Diagram: Droplets", show=False, filename="my-diagram", direction="LR"):
+    with Cluster("DigitalOcean"):
+        droplet1 = Droplet("My first droplet")
+        droplet2 = Droplet("My second droplet")
 
-with Diagram("Simple Diagram", show=False, graph_attr=graph_attr):
-    EC2("web")
+    db = DbaasPrimary("My database")
+    
+    logstash = Logstash("Logstash service")
+    
+    [droplet1, droplet2] >> db >> [droplet1, droplet2]
+    [droplet1, droplet2, db] >> Edge(color="firebrick", style="dashed") >> logstash
