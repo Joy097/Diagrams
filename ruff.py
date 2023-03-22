a5ffeee27a8a33fe52f25813135594582dc71f68
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-22 15:12:36 +0600

    update 30

--- ruff.py
+++ ruff.py
@@ -1,14 +1,16 @@
 from diagrams import Cluster, Diagram
+from shapes import Shape
 
+# Define a custom shape
+class CustomShape(Shape):
+    _shape = "custom_shape"
+    _template = "<rect x='0' y='0' width='100' height='50' rx='10' ry='10' />"
+
+# Create a diagram
 with Diagram("My Diagram") as diag:
-    with Cluster("My Cluster", shape="box", bgcolor="gray"):
-        # Define the interface
-        interface = diag.add("Interface", shape="cylinder", style="dashed")
-        
-        # Add other objects inside the cluster
+    # Create a cluster
+    with Cluster("My Cluster", shape=CustomShape()):
+        # Add objects inside the cluster
         object1 = diag.add("Object 1")
         object2 = diag.add("Object 2")
 
-        # Link the interface to other objects
-        interface >> object1
-        interface >> object2
