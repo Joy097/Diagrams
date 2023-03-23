7dece7101bbfed3499f45ee02d6fd0b35a5b3111
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-23 09:35:02 +0600

    update 5

--- ruff.py
+++ ruff.py
@@ -3,8 +3,8 @@ from diagrams.aws.compute import EC2
 
 
 with Diagram("Example Diagram"):
-    with Cluster("Example Cluster", graph_attr={"pos": "10,10!"}):
-        ec2 = EC2("Example EC2 Instance")
+    with Cluster("Example Cluster"):
+        ec2 = EC2("Example EC2 Instance",node_attr="pos=\"100,10!\"")
     
 
 
