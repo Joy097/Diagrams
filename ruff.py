9a2f43cc54214747fbd13f5275666b2dd58d6b47
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-23 10:45:21 +0600

    update 12

--- ruff.py
+++ ruff.py
@@ -1,6 +1,14 @@
-from diagrams import Diagram, Cluster
+from diagrams import Cluster, Diagram
 from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import VPC
 
-with Diagram("Example Diagram") as diag:
-    with Cluster("Example Cluster", graph_attr={"pos": "10,10!"}, subgraph_attr={"pos": "5,5!"}):
-        ec2 = EC2("Example EC2 Instance")
+with Diagram("Example Diagram", show=False):
+    with Cluster("VPC"):
+        with Cluster("Private Subnet", label="Database Servers"):
+            rds = RDS("Example RDS Instance")
+
+        with Cluster("Public Subnet"):
+            # ... add resources to public subnet cluster ...
+
+        VPC("Example VPC") >> Cluster("Database Servers", group="databases") >> rds
