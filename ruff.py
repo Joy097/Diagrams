cafb0b77f71436b2865e0d7b3650e5c9c876a3d0
Joy097 <shihabrumeechowdhury@gmail.com>
2023-03-23 09:24:59 +0600

    update 4

--- ruff.py
+++ ruff.py
@@ -1,9 +1,10 @@
 from diagrams import Diagram, Cluster
-from diagrams.utils import save
 from diagrams.aws.compute import EC2
 
 
 with Diagram("Example Diagram"):
     with Cluster("Example Cluster", graph_attr={"pos": "10,10!"}):
         ec2 = EC2("Example EC2 Instance")
-    save("example-diagram.svg")
+    
+
+
